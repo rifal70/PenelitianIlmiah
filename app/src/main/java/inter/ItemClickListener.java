6152114615c860afd1a82fddd5cf5b87a207381d
rifal70 <60748878+rifal70@users.noreply.github.com>
2020-02-11 16:42:07 +0700

    1

---
+++ app/src/main/java/inter/ItemClickListener.java
@@ -0,0 +1,11 @@
+package inter;
+
+import android.view.View;
+
+/**
+ * Created by mohamed on 11/21/17.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view,int position,boolean isLongClick);
+}
